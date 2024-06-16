42829a399dab2707bc3f12a11dcb2ddf3045cc13
Parin Lai <parinll@Parins-MacBook-Pro.local>
2024-06-16 14:01:17 +0800

    chore: remove unnessesary quotes

--- summarize-text-with-bedrock.py
+++ summarize-text-with-bedrock.py
@@ -51,38 +51,3 @@ for event in streaming_response["body"]:
     chunk = json.loads(event["chunk"]["bytes"])
     if chunk["type"] == "content_block_delta":
         print('\033[97;40m' + chunk["delta"].get("text", "")+ '\033[0m', end="")
-
-# # Extract and print the response text in real-time.
-# for event in streaming_response["body"]:
-#     chunk = json.loads(event["chunk"]["bytes"])
-#     if chunk["type"] == "content_block_delta":
-#         print(chunk["delta"].get("text", ""), end="")
-
-
-
-# try:
-#     response = client.invoke_model(
-#         body = bytes(json.dumps({
-#             "max_tokens": 2048,
-#             "temperature": 1.0,
-#             "anthropic_version": "bedrock-2023-05-31",
-#             "messages": [
-#                 {
-#                     "role": "user",
-#                     "content": [{"type": "text", "text": prompt}],
-#                 }
-#             ],
-#         }), 'utf-8'),
-#         modelId = "anthropic.claude-3-haiku-20240307-v1:0",
-#         # modelId = "anthropic.claude-instant-v1",
-#         contentType = "application/json",
-#         accept = "application/json"
-#     )
-#     response_text = response['body'].read().decode('utf8').strip()
-#     response_json = json.loads(response_text)
-#     completion = response_json['completion']
-#     print('\033[97;40m' + completion + '\033[0m')
-# except Exception as e:
-#     print('Error invoking endpoint')
-#     print(e)
-#     sys.exit(1)
