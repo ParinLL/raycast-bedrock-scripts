d679c3046bf0d94589dae920e66ed574907b72d7
Parin Lai <parinlai+onboarding@amazon.com>
2024-11-10 16:13:51 +0800

    chore: change model ID to anthropic.claude-3-5-sonnet-20241022-v2:0

--- source/translate-article-to-english.py
+++ source/translate-article-to-english.py
@@ -3,7 +3,7 @@ import boto3
 import json
 import sys
 
-def get_summary(text, model_id="anthropic.claude-3-5-sonnet-20240620-v1:0", max_tokens=4096, temperature=1.0):
+def get_summary(text, model_id="anthropic.claude-3-5-sonnet-20241022-v2:0", max_tokens=4096, temperature=1.0):
     client = boto3.client('bedrock-runtime', region_name='ap-northeast-1')
     
     prompt = f"""
